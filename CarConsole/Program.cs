b3da0233c1b0207020ffe41d9e26284689baab54
szabomarcel <szabomarcell34@gmail.com>
2024-03-06 15:52:46 +0100

    hazi_feladat

---
+++ CarConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Car;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CarConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await car();
+            Console.WriteLine("Car name: ");
+            Console.ReadLine();
+        }
+
+        private static async Task car()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            //Console.WriteLine(await response.Content.ReadAsStringAsync()); // Ez íratná li az egész API server adatait 
+            var jsonString = await response.Content.ReadAsStringAsync();
+            // Deszerializálja a JSON-választ  => // sorba rendezés.
+            var jsonObject = JObject.Parse(jsonString);
+            var results = jsonObject["Results"];
+
+            // Név alapján van a szűrés
+            foreach (var result in results)
+            {
+                Console.WriteLine(result["Make_Name"]);
+            }
+        }
+    }
+}
